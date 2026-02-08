b45f0354a8318992f588ab9fd5d4015b1fe6308e
jacques-navarro <4421229+jacques-navarro@users.noreply.github.com>
2026-02-08 21:48:01 +0100

    Add context variables, update home template

--- pages/views.py
+++ pages/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render
 
 def home_page_view(request):
-  return render(request, "home.html")
+  context = {
+    "inventory_list": ["widget 1", "widget 2", "widget 3"],
+    "greeting": "ThAnk YoU fOr ViSiTinG.",
+  }
+  return render(request, "home.html", context)
 
 
