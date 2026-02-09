87324f26fc5e1644ca6f44a64c8b73982bdd2582
jacques-navarro <4421229+jacques-navarro@users.noreply.github.com>
2026-02-09 06:30:51 +0100

    Test correct template is used

--- pages/tests.py
+++ pages/tests.py
@@ -10,6 +10,10 @@ class HomePageTests(SimpleTestCase):
     response = self.client.get(reverse("home"))
     self.assertEqual(response.status_code, 200)
 
+  def test_template_name_correct(self):
+    response = self.client.get(reverse("home"))
+    self.assertTemplateUsed(response, "home.html")
+
 class AboutPageTests(SimpleTestCase):
   def test_url_exists_at_correct_location(self):
     response = self.client.get("/about/")
@@ -18,3 +22,7 @@ class AboutPageTests(SimpleTestCase):
   def test_url_available_by_name(self):
     response = self.client.get(reverse("about"))
     self.assertEqual(response.status_code, 200)
+
+  def test_template_name_correct(self):
+    response = self.client.get(reverse("about"))
+    self.assertTemplateUsed(response, "about.html")
